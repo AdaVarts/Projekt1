f4bedaa604e24c7dd613a87de29cea22f82ee125
Klisa <klisa.elizaveta@yandex.ru>
2018-11-07 10:48:29 +0100

    com r

--- Projekt1/Source.cpp
+++ Projekt1/Source.cpp
@@ -22,21 +22,22 @@ driver_t** drivers = NULL;
 
 void o() {
 	f = fopen("priestupky.txt", "r");
-	count = 0;
+	int j = 0;
 	char str[265];
 	while (fgets(str, 265, f)>0) {
-		count++;
+		j++;
 	}
+	count = (j + 1) / 8;
 	fclose(f);
 	f = fopen("priestupky.txt", "r");
 	
 	
 	if (drivers != NULL) {
-		for (int i = 0; i < (count + 1) / 8; i++)
+		for (int i = 0; i < count; i++)
 			free(drivers[i]);
 	}
 	drivers = (driver_t**)malloc(sizeof(driver_t*));
-	for (int j = 0; j < (count + 1) / 8; j++)
+	for (int j = 0; j < count; j++)
 		drivers[j] = (driver_t*)malloc(sizeof(driver_t));
 	if (drivers != NULL) {
 		int i = 0;
@@ -83,7 +84,7 @@ void o() {
 
 void v() {
 	if (drivers != NULL) {
-		for (int i = 0; i < (count + 1) / 8; i++) {
+		for (int i = 0; i < count; i++) {
 			printf("meno priezvisko: %s\n", drivers[i]->name);
 			printf("pohlavie: %c\n", drivers[i]->sex);
 			printf("rok narodenia: %d\n", drivers[i]->birthday);
@@ -104,7 +105,7 @@ void x() {
 	
 	int year = ptr->tm_year + 1900;
 
-	for (int i = 0; i < (count+1)/8; i++)
+	for (int i = 0; i < count; i++)
 	{
 		char str[5];
 		strncpy(str, drivers[i]->dateOfFine, 4);
@@ -118,7 +119,7 @@ void x() {
 
 void p() {
 	if (drivers != NULL) {
-		for (int i = 0; i < (count + 1) / 8; i++)
+		for (int i = 0; i < count; i++)
 		{
 			char str[8];
 			for (int j = 0; j < 7; j++)
@@ -133,24 +134,29 @@ void p() {
 
 void r() {
 	if (drivers != NULL) {
-		struct tm *ptr;
-		time_t lt;
+		
+		char *str;
+		str = (char*)malloc(9);
+		scanf("%8s", str);
+		char year[5];
+		strncpy(year, str, 4);
+		year[4] = '\0';
+		char month[3];
+		month[0] = str[4];
+		month[1] = str[5];
+		month[2] = '\0';
 
-		lt = time(NULL);
-		ptr = localtime(&lt);
-
-		int month = ptr->tm_mon + 1;
-		int year = ptr->tm_year + 1900;
 		double odmena = 0;
-		for (int i = 0; i < (count + 1) / 8; i++)
+		for (int i = 0; i < count; i++)
 		{
-			char str[5];
+			char str1[5];
 			char str2[3];
-			strncpy(str, drivers[i]->dateOfFine, 4);
+			strncpy(str1, drivers[i]->dateOfFine, 4);
+			str1[4] = '\0';
 			str2[0] = drivers[i]->dateOfFine[4];
 			str2[1] = drivers[i]->dateOfFine[5];
 			str2[2] = '\0';
-			if (atoi(str) == year && atoi(str2) == month) {
+			if (strcmp(str1, year) == 0 && strcmp(str2, month) == 0) {
 				if (drivers[i]->offence == 1)
 					odmena += drivers[i]->sumOfFine*0.052;
 				else if (drivers[i]->offence == 0)
@@ -161,6 +167,39 @@ void r() {
 			printf("%.2lf\n", odmena);
 	}
 }
+
+/*void a() {
+	if (drivers == NULL) {
+		drivers = (driver_t**)malloc(sizeof(driver_t*));
+		drivers[0] = (driver_t*)malloc(sizeof(driver_t));
+		count++;
+	}
+	else {
+		driver_t** drivers1 = (driver_t**)malloc(sizeof(driver_t*));
+		for (int i = 0; i < count + 1; i++) {
+			drivers1[i] = (driver_t*)malloc(sizeof(driver_t));
+			if (i != count) {
+				strcpy(drivers1[i]->name, drivers[i]->name);
+				drivers1[i]->sex = drivers[i]->sex;
+				drivers1[i]->birthday = drivers[i]->birthday;
+				strcpy(drivers1[i]->plate, drivers[i]->plate);
+				drivers1[i]->offence = drivers[i]->offence;
+				drivers1[i]->sumOfFine = drivers[i]->sumOfFine;
+				strcpy(drivers1[i]->dateOfFine, drivers[i]->dateOfFine);
+
+				free(drivers[i]);
+			}
+		}
+		count++;
+	}
+	scanf_s("%s", drivers[count - 1]->name);
+	scanf_s("%c", drivers[count - 1]->sex);
+	scanf_s("%d", drivers[count - 1]->birthday);
+	scanf_s("%s", drivers[count - 1]->plate);
+	scanf_s("%d", drivers[count - 1]->offence);
+	scanf_s("%d", drivers[count - 1]->sumOfFine);
+	scanf_s("%s", drivers[count - 1]->dateOfFine);
+}*/
 
 void k() {
 	if (f != NULL)
@@ -194,6 +233,9 @@ int main() {
 			break;
 		case 'v':
 			v();
+			break;
+		case 'a':
+			//a();
 			break;
 		default:
 			break;
